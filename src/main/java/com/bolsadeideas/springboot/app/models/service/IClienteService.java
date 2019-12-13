459bf7d52d768b14b3501a2c00702ceedee8ee5b
leanysp <leanysp@gmail.com>
2019-12-13 17:48:35 -0500

    Paginador

--- src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
@@ -2,6 +2,9 @@ package com.bolsadeideas.springboot.app.models.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
 public interface IClienteService {
@@ -10,6 +13,13 @@ public interface IClienteService {
 	 * @return
 	 */
 	public List<Cliente> findAll();
+	
+	/**
+	 * 
+	 * @param pageable
+	 * @return
+	 */
+	public Page<Cliente> findAll(Pageable pageable);
 	
 	/**
 	 * metodo para guardar u nuevo cliente
