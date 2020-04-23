c325a58bd5030b8e14a53a2a49a976730a3c29bf
leanysp <leanysp@gmail.com>
2020-04-23 15:59:35 -0500

    Spring security, login y logout

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -25,5 +25,8 @@ public class MvcConfig implements WebMvcConfigurer{
 		.addResourceLocations(resourcePath);	
 	}*/
 	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/error_403").setViewName("error_403");;
+	}
 
 }
