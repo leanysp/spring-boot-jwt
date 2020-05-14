f0e22e31a9903362a0aaf6db7417003462cc68fd
leanysp <leanysp@gmail.com>
2020-05-14 12:34:44 -0500

    lineas de la factura en la vista PDF

--- src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
+++ src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.bolsadeideas.springboot.app.models.entity.Factura;
+import com.bolsadeideas.springboot.app.models.entity.ItemFactura;
 import com.lowagie.text.Document;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
@@ -37,6 +40,27 @@ public class FacturaPdfView extends AbstractPdfView{
 		
 		document.add(tabla);
 		document.add(tabla2);
+		
+		PdfPTable tabla3 = new PdfPTable(4);
+		tabla3.addCell("Producto");
+		tabla3.addCell("Precio");
+		tabla3.addCell("Cantidad");
+		tabla3.addCell("Total");
+		
+		for(ItemFactura item: factura.getItems()) {
+			tabla3.addCell(item.getProducto().getNombre());
+			tabla3.addCell(item.getProducto().getPrecio().toString());
+			tabla3.addCell(item.getCantidad().toString());
+			tabla3.addCell(item.calcularImporte().toString());
+		}
+		
+		PdfPCell cell = new PdfPCell(new Phrase("Total: "));
+		cell.setColspan(3);
+		cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+		tabla3.addCell(cell);
+		tabla3.addCell(factura.getTotal().toString());
+		
+		document.add(tabla3);
 		
 	}
 
