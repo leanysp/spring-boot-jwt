dc6ff5aabbb989ee29078d11adf9764a0cea569c
leanysp <leanysp@gmail.com>
2020-06-18 17:32:37 -0500

    Agrergando mas datos en el token jwt

--- src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.bolsadeideas.springboot.app.auth.filter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,12 +16,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -64,10 +68,18 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 			Authentication authResult) throws IOException, ServletException {
 
 		String username = ((User) authResult.getPrincipal()).getUsername();
+		Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+		Claims claims = Jwts.claims();
+		claims.put("authorities", new ObjectMapper().writeValueAsString(roles));
 		
 		SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 		
-        String token = Jwts.builder().setSubject(username).signWith(secretKey).compact();
+        String token = Jwts.builder()
+        		.setClaims(claims)
+        		.setSubject(username).signWith(secretKey)
+        		.setIssuedAt(new Date())
+        		.setExpiration(new Date(System.currentTimeMillis() + 3600000*4L))
+        		.compact();
         
         response.addHeader("Authorization", "Bearer" + token);
 		Map<String, Object> body = new HashMap<String, Object>();
